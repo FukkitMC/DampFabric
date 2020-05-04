3f9ad0f44007f0cff4d4bc6175c8ec484a81af0d
hYdos <haydenv06@gmail.com>
2020-05-04 16:37:32 +1000

    by seeing the msg u cant sue me

---
+++ src/main/java/me/hydos/dampfabric/DampFabric.java
@@ -0,0 +1,11 @@
+package me.hydos.dampfabric;
+
+import net.fabricmc.api.ModInitializer;
+
+public class DampFabric implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+
+    }
+}
