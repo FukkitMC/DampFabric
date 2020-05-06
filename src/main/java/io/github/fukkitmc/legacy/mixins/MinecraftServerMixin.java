5b68988ea5f5a1e5bec8fea2a75a8f824af1163a
hYdos <haydenv06@gmail.com>
2020-05-06 19:17:44 +1000

    someone do the main method please i cant just no no no no no no no no no no no no no no no no

--- src/main/java/io/github/fukkitmc/legacy/mixins/MinecraftServerMixin.java
+++ src/main/java/io/github/fukkitmc/legacy/mixins/MinecraftServerMixin.java
@@ -1,11 +1,20 @@
 package io.github.fukkitmc.legacy.mixins;
 
+import io.github.fukkitmc.legacy.CursedOptionLoader;
 import io.github.fukkitmc.legacy.extra.MinecraftServerExtra;
-import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.PlayerList;
-import net.minecraft.server.UserCache;
+import joptsimple.OptionSet;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.server.*;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.io.File;
+import java.net.Proxy;
 
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin implements MinecraftServerExtra {
@@ -15,6 +24,45 @@ public class MinecraftServerMixin implements MinecraftServerExtra {
     @Shadow public UserCache Z;
 
     @Shadow public PlayerList v;
+
+
+    @Inject(method = "<init>(Ljava/io/File;Ljava/net/Proxy;Ljava/io/File;)V", at=@At("TAIL"))
+    public void cursedConstructor(File file, Proxy proxy, File file2, CallbackInfo ci){
+        ((MinecraftServer)(Object)this).options = CursedOptionLoader.bukkitOptions;
+    }
+
+    /**
+     * @author
+     */
+    @Environment(EnvType.SERVER)
+    @Overwrite
+    public static void main(String[] args){
+        OptionSet options = CursedOptionLoader.loadOptions(args);
+        DispenserRegistry.c();
+
+        String string2 = ".";
+
+        DedicatedServer dedicatedServer = new DedicatedServer(new File(string2));
+
+//        if (options.has("port")) {
+//            int port = (Integer) options.valueOf("port");
+//            if (port > 0) {
+//                dedicatedserver.setPort(port);
+//            }
+//        }
+
+        dedicatedServer.D();
+        Runtime.getRuntime().addShutdownHook(new Thread(dedicatedServer::t, "Server Shutdown Thread"));
+
+        if (options.has("universe")) {
+            dedicatedServer.universe = (File) options.valueOf("universe");
+        }
+
+//        if (options.has("world")) {
+//            dedicatedserver.setWorld((String) options.valueOf("world"));
+//        }
+
+    }
 
     @Override
     public int getIdleTimeout() {
@@ -50,7 +98,7 @@ public class MinecraftServerMixin implements MinecraftServerExtra {
 
     @Override
     public void safeShutdown() {
-        //this.isRunning = false;
+//        this.isRunning = false;
     }
 
     @Override
